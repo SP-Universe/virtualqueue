749948f0b45d621cd143f1f59f559f75bda79b3d
Steffen Kahl <stevenpaw209@gmail.com>
2022-09-28 01:16:19 +0200

    [CHANGED] Fixed Display

--- display/index.php
+++ display/index.php
@@ -14,7 +14,7 @@
     <h1>Willkommen beim Halloweenhaus Schmalenbeck!</h1>
     <h2 class="current_group">Aktuelle Gruppe: <?php echo $current_group;?></h2>
 
-    <div class="display_cards">
+    <div class="display_cards" id="reloading">
         <div class="display_row">
             <div class="display_row_block">
                 <div class="display_guests">
@@ -32,8 +32,14 @@
                     ?>
                 </div>
             </div>
-            <div class="display_row_block">
-                <p class="big_number"><?php echo ($max_vq_users_per_group + $min_sq_users_per_group) - get_data_from_group($current_group, "vq_guests") - get_data_from_setting("display_guest_count");?></p>
+            <div class="display_row_block" >
+                <p class="big_number"><?php 
+                    if(($max_vq_users_per_group + $min_sq_users_per_group) - get_data_from_group($current_group, "vq_guests") - get_data_from_setting("display_guest_count") > 0){
+                        echo ($max_vq_users_per_group + $min_sq_users_per_group) - get_data_from_group($current_group, "vq_guests") - get_data_from_setting("display_guest_count");
+                    } else {
+                        echo 0;
+                    }
+                ?></p>
                 <p>Gäste aus der standby Queue</p>
             </div>
         </div>
@@ -42,9 +48,17 @@
 
 </body>
 
-<script>
+<!-- <script>
     setInterval(function(){ 
 	    window.location.reload();
     }, 4000);
 
+</script> -->
+<script src="jquery-3.6.0.min.js"></script>
+<script>
+   $(document).ready(function(){
+   window.setInterval(function(){
+     $("#reloading").load(window.location.href + " #reloading" );
+   }, 3000);
+   });
 </script>
