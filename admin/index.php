933cb9f6f05983edc4e6354c5d768ee13e097e36
Steffen Kahl <stevenpaw209@gmail.com>
2022-10-05 23:05:06 +0200

    [CHANGED] Countdown to be better

--- admin/index.php
+++ admin/index.php
@@ -277,8 +277,11 @@ console.log(countDownDate);
 setInterval(function() {
     var now = new Date().getTime();
     var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    document.getElementById("admin_next_group_countdown").innerHTML = "<p>" + seconds + " Sekunden bis zum nächstem Einlass</p>";
+    document.getElementById("admin_next_group_countdown").innerHTML = "<p>" + hours + ":" + minutes + ":" + seconds + " Sekunden bis zum nächstem Einlass</p>";
     console.log(countDownDate);
 }, 1000);
 
