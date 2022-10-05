baeddc01ab8c96bfaf95edf26c180a5f38b7ea31
Steffen Kahl <stevenpaw209@gmail.com>
2022-10-05 22:00:19 +0200

    [CHANGED] Readded normal code

--- userpage.php
+++ userpage.php
@@ -6,13 +6,13 @@
     if($guestid === null)
     {
         if (isset($_GET['new_guests'])) {
-            //header("Location: index.php?hint=new_user&guestid=" . $guestid);
+            header("Location: index.php?hint=new_user&guestid=" . $guestid);
             if($_GET['new_guests'] <= get_data_from_setting("max_vq") && $_GET['new_guests'] > 0){
                 $guestid = add_new_guest($_GET['new_guests']);
                 echo $guestid;
                 setusercookie($guestid);
             } else {
-                //header("Location: index.php?error=Too many people in your group!");
+                header("Location: index.php?error=Too many people in your group!");
                 close_connection();
                 exit;
             }
@@ -20,9 +20,9 @@
     }
     else if(checkforexistingid($guestid))
     {
-        //header("Location: index.php?hint=existing_user");
+        header("Location: index.php?hint=existing_user");
     } else {
-        //header("Location: index.php?error=Unbekannter Gast! " . $guestid);
+        header("Location: index.php?error=Unbekannter Gast! " . $guestid);
     }
     close_connection();
     exit;
