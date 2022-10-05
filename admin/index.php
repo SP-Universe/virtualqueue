54f24e4e970d765ffac821b695462b23eea603c3
Steffen Kahl <stevenpaw209@gmail.com>
2022-10-06 00:31:58 +0200

    [CHANGED] Fixed ids

--- admin/index.php
+++ admin/index.php
@@ -49,8 +49,7 @@
 
         foreach(get_all_groups() as $group){
             if($group['groupid'] > $current_group){
-                echo $group['groupid'];
-                recalculateTimes($group['id']);
+                recalculateTimes($group['groupid']);
             }
         }
 
